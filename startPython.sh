281e3312f2782168160d1f3b8f5e79d4a5bcfdd5
LostBaboon <domotic70@gmail.com>
2022-04-27 19:48:10 +0200

    Update startPython.sh

--- startPython.sh
+++ startPython.sh
@@ -4,17 +4,15 @@ clear
 echo -e " PimpMySuperWatt python direct installation\n"
 echo -e " Check prequisites... \n\n"
 echo -e " - Checking python version need 3.7 : \c"
-VERSION=$(test -f /usr/bin/python3.7 && echo 1 || echo 0)
+VERSION=$(python -V | grep -ci "3.7")
 if [ "${VERSION}" = "0" ]
  then
   echo "KO $(python -V 2>&1)"
   echo -e "   --> Please override PATH for prior python 3.7\n"
   echo -e "       export PATH=/usr/lib/python3/bin:\$PATH ? \n"
   exit 1
-
- else
-  echo "OK"
 fi
+
 echo -e " - Cleaning .venv python : \c"
 rm -rf .venv 2>/dev/null
 if [ "$?" -eq 0 ]
